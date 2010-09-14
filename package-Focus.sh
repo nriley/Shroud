611c817014a2a5933c6ee2e7aed390e2b16ea4dc
Nicholas Riley <njriley@illinois.edu>
2010-09-14 07:01:26 -0500

    agvtool and Sparkle in packaging script.

--- package-Focus.sh
+++ package-Focus.sh
@@ -4,7 +4,8 @@ PACKAGEDIR="$PWD"
 PRODUCT="Focus"
 
 # gather information
-VERSION=$(/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' "$PRODUCT-Info.plist")
+VERSION=`agvtool mvers -terse1`
+BUILD=`agvtool vers -terse`
 DMG="$PRODUCT-$VERSION.dmg" VOL="$PRODUCT $VERSION"
 DSTROOT="$PACKAGEDIR/$VOL"
 
@@ -14,7 +15,7 @@ find . -name \*~ -exec rm '{}' \;
 rm -rf build/
 xcodebuild -target Focus -configuration Release DSTROOT="$DSTROOT" \
     INSTALL_PATH=/ DEPLOYMENT_LOCATION=YES install
-rm -rf "$DSTROOT/ShortcutRecorder"*
+rm -rf "$DSTROOT/ShortcutRecorder"* "$DSTROOT/Sparkle"*
 
 # create disk image
 cd "$PACKAGEDIR"
@@ -31,3 +32,17 @@ hdiutil convert $DMG -format UDBZ -o z$DMG
 mv z$DMG $DMG
 hdiutil internet-enable $DMG
 chmod 644 $DMG
+zmodload zsh/stat
+SIZE=$(stat -L +size $DMG)
+
+# update Web presence
+DIGEST=`openssl dgst -sha1 -binary < $DMG | openssl dgst -dss1 -sign ~/Documents/Development/DSA/dsa_priv.pem | openssl enc -base64`
+NOW=`perl -e 'use POSIX qw(strftime); print strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())) . $tz'`
+perl -pi -e 's|(<enclosure url=".+'$DMG'").+/>|\1 length="'$SIZE'" type="application/x-apple-diskimage" sparkle:version="'$BUILD'" sparkle:shortVersionString="'$VERSION'" sparkle:dsaSignature="'$DIGEST'"/>|' Updates/updates.xml
+perl -pi -e 's#<(pubDate|lastBuildDate)>[^<]*#<$1>'$NOW'# && $done++ if $done < 3' Updates/updates.xml
+perl -pi -e 's|(<guid isPermaLink="false">)[^<]*|$1'${PRODUCT:l}-${VERSION:s/.//}'| && $done++ if $done < 1' Updates/updates.xml
+scp $DMG ainaz:web/nriley/software/$DMG.new
+ssh ainaz chmod go+r web/nriley/software/$DMG.new
+ssh ainaz mv web/nriley/software/$DMG{.new,}
+rsync -avz --exclude='.*' Updates/ ainaz:web/nriley/software/$PRODUCT/
+ssh ainaz chmod -R go+rX web/nriley/software/$PRODUCT
