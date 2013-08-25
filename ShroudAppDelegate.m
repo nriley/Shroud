eb97a6e73ecfeb9f7f5401e645cbba8ab3b83f03
Nicholas Riley <com-github@sabi.net>
2013-08-24 23:54:43 -0500

    Work around OS X bug in which the Credits text is not centered.
    <http://www.openradar.me/14829080>

--- ShroudAppDelegate.m
+++ ShroudAppDelegate.m
@@ -341,9 +341,37 @@ static ProcessSerialNumber frontProcess;
     NSImage *image = [[NSImage alloc] init];
     [image addRepresentation: imageRep];
 
+    // XXX work around bug in OS X 10.7 / 10.8 where the Credits text is not centered (r. 14829080)
+    NSSet *windowsBefore = [NSSet setWithArray:[NSApp windows]];
+
     [NSApp orderFrontStandardAboutPanelWithOptions:
      [NSDictionary dictionaryWithObject:image forKey:@"ApplicationIcon"]];
     [image release];
+
+    for (NSWindow *window in [NSApp windows]) {
+        if ([windowsBefore containsObject:window])
+            continue;
+
+        for (NSView *view in [[window contentView] subviews]) {
+            if (![view isKindOfClass:[NSScrollView class]])
+                continue;
+
+            NSClipView *clipView = [(NSScrollView *)view contentView];
+            NSRect clipViewFrame = [clipView frame];
+            NSView *documentView = [clipView documentView];
+            NSRect documentViewFrame = [documentView frame];
+
+            if (clipViewFrame.size.height != documentViewFrame.size.height)
+                continue; // don't mess with a scrollable view
+
+            if (clipViewFrame.size.width != documentViewFrame.size.width) {
+                documentViewFrame.size.width = clipViewFrame.size.width;
+                [documentView setFrame:documentViewFrame];
+                break;
+            }
+        }
+        break;
+    }
 }
 
 - (IBAction)orderFrontPreferencesPanel:(id)sender;
