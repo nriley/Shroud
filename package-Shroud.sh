55ad1c260c3bd7769a6e0a8a9a6b57f7632f9d1a
Nicholas Riley <com-github@sabi.net>
2018-10-18 19:01:46 -0400

    Packaging script changes, mostly from NewsBlur Helper via Pester.
    Use modern archiving process rather than ancient install hack.
    
    Sign the disk image to avoid translocation.
    
    Built products and archives now end up in their own directories.

--- package-Shroud.sh
+++ package-Shroud.sh
@@ -1,60 +1,88 @@
 #!/bin/zsh -e
 
-PACKAGEDIR="$PWD"
-PRODUCT="Shroud"
+set -x -v
+
+SOURCEDIR="$PWD"
+ARCHIVEDIR="$PWD"/Archives
+EXPORTDIR=$(mktemp -d)
+RELEASEDIR="$PWD"/Releases
+UPDATEDIR="$PWD"/Updates
+PRODUCT=$(print "$SOURCEDIR"/*.xcodeproj(:t:r))
 
 # gather information
-VERSION=`agvtool mvers -terse1`
-BUILD=`agvtool vers -terse`
-DMG="$PRODUCT-$VERSION.dmg" VOL="$PRODUCT $VERSION"
-DSTROOT="$PACKAGEDIR/$VOL"
-SYMROOT="$PWD/build"
+cd $SOURCEDIR
+VERSION=$(agvtool mvers -terse1)
+BUILD=$(agvtool vers -terse)
+DMG="$RELEASEDIR/$PRODUCT-$VERSION.dmg"
+VOL="$PRODUCT $VERSION"
+ARCHIVE="$ARCHIVEDIR/$PRODUCT $VERSION ($BUILD).xcarchive"
+EXPORT="$EXPORTDIR/$VOL"
 
-# clean and build
-find . -name \*~ -exec rm '{}' \;
-rm -rf "$SYMROOT" "$DSTROOT"
-# can't have INSTALL_PATH=/ for bundled frameworks because it'll override @executable_path etc.
-xcodebuild -target "$PRODUCT" -configuration Release DSTROOT="$DSTROOT" \
-    SYMROOT="$SYMROOT" DEPLOYMENT_LOCATION=YES install
+# archive and export
+mkdir -p $ARCHIVEDIR
+rm -rf $ARCHIVE
+xcodebuild -scheme $PRODUCT -archivePath $ARCHIVE archive
+xcodebuild -archivePath $ARCHIVE -exportArchive -exportPath $EXPORT -exportOptionsPlist $SOURCEDIR/exportOptions.plist
 
 # ensure code signature and Developer ID are valid
-codesign --verify --verbose=4 "$VOL"/*.app
-spctl -vv --assess "$VOL"/*.app
+codesign --verify --verbose=4 "$EXPORT"/*.app
+# also capture the identity in order to sign the disk image
+IDENTITY=$(spctl -vv --assess "$EXPORT"/*.app 2>&1 | grep 'origin=' | sed -e 's/^origin=//')
+
+# remove export metadata we don't want in the disk image
+rm -f $EXPORT/*.plist $EXPORT/Packaging.log
 
 # create disk image
-cd "$PACKAGEDIR"
+mkdir -p $RELEASEDIR
 rm -f $DMG
-hdiutil create $DMG -megabytes 5 -ov -layout NONE -fs 'HFS+' -volname $VOL
-MOUNT=`hdiutil attach $DMG`
-DISK=`echo $MOUNT | sed -ne ' s|^/dev/\([^ ]*\).*$|\1|p'`
-MOUNTPOINT=`echo $MOUNT | sed -ne 's|^.*\(/Volumes/.*\)$|\1|p'`
-ditto -rsrc "$DSTROOT" "$MOUNTPOINT"
+hdiutil create $DMG -megabytes 20 -ov -layout NONE -fs 'HFS+' -volname $VOL
+MOUNT=$(hdiutil attach $DMG)
+DISK=$(echo $MOUNT | sed -ne ' s|^/dev/\([^ ]*\).*$|\1|p')
+MOUNTPOINT=$(echo $MOUNT | sed -ne 's|^.*\(/Volumes/.*\)$|\1|p')
+ditto -rsrc "$EXPORT" "$MOUNTPOINT"
 chmod -R a+rX,u+w "$MOUNTPOINT"
 hdiutil detach $DISK
 hdiutil resize -sectors min $DMG
-hdiutil convert $DMG -format UDBZ -o z$DMG
-mv z$DMG $DMG
+ZDMG="${DMG:r}z.dmg"
+hdiutil convert $DMG -format UDBZ -o $ZDMG
+mv $ZDMG $DMG
 hdiutil internet-enable $DMG
-chmod 644 $DMG
+
+# sign the disk image
+codesign --sign $IDENTITY $DMG
+
+# verify disk image signature
+spctl -vv --assess --type open --context context:primary-signature $DMG
+
 zmodload zsh/stat
 SIZE=$(stat -L +size $DMG)
 
-# update Web presence
+# clean up
+rm -rf "$EXPORTDIR"
+
+# update appcast
+APPCAST="$UPDATEDIR"/updates.xml
 DIGEST=`openssl dgst -sha1 -binary < $DMG | openssl dgst -dss1 -sign ~/Documents/Development/DSA/dsa_priv.pem | openssl enc -base64`
 NOW=`perl -e 'use POSIX qw(strftime); print strftime("%a, %d %b %Y %H:%M:%S %z", localtime(time())) . $tz'`
-if ! grep -q "<title>$PRODUCT $VERSION" Updates/updates.xml; then
-    print "can't find $PRODUCT $VERSION - please duplicate <item> at top of appcast and change its <title>"
-    exit 1
+perl -pi -e 's|(<enclosure url=".+'${DMG:t}'").+/>|\1 length="'$SIZE'" type="application/x-apple-diskimage" sparkle:version="'$BUILD'" sparkle:shortVersionString="'$VERSION'" sparkle:dsaSignature="'$DIGEST'"/>|' $APPCAST
+perl -pi -e 's#<(pubDate|lastBuildDate)>[^<]*#<$1>'$NOW'# && $done++ if $done < 3' $APPCAST
+perl -pi -e 's|(<guid isPermaLink="false">)[^<]*|$1'${PRODUCT:l}-${VERSION:s/.//}'| && $done++ if $done < 1' $APPCAST
+
+if [[ -n $1 ]]; then
+    return
 fi
-perl -pi -e 's|(<enclosure url="[^"]*/)[^"]*"[^>]*>|\1'$DMG'" length="'$SIZE'" type="application/x-apple-diskimage" sparkle:version="'$BUILD'" sparkle:shortVersionString="'$VERSION'" sparkle:dsaSignature="'$DIGEST'"/>| && $done++ if $done < 1' Updates/updates.xml
-perl -pi -e 's#<(pubDate|lastBuildDate)>[^<]*#<$1>'$NOW'# && $done++ if $done < 3' Updates/updates.xml
-perl -pi -e 's|(<guid isPermaLink="false">)[^<]*|$1'${PRODUCT:l}-${VERSION:s/.//}'| && $done++ if $done < 1' Updates/updates.xml
-scp $DMG osric:web/nriley/software/$DMG.new
-ssh osric chmod go+r web/nriley/software/$DMG.new
-ssh osric mv web/nriley/software/$DMG{.new,}
+
+# update Web presence
+WEBDIR=web/nriley/software
+WEBDMG=$WEBDIR/${DMG:t}
+WEBAPPCAST=$WEBDIR/${APPCAST:t}
+scp $DMG osric:${WEBDMG:q}.new
+ssh osric chmod go+r ${WEBDMG:q}.new
+ssh osric mv ${WEBDMG:q}{.new,}
 # for testing
-mv Updates/updates.xml Updates/updatez.xml
-rsync -avz --exclude='.*' Updates/ osric:web/nriley/software/$PRODUCT/
+APPCAST_TESTING="$UPDATEDIR"/updatez.xml
+mv $APPCAST $APPCAST_TESTING
+rsync -avz --exclude='.*' $UPDATEDIR/ osric:$WEBDIR/$PRODUCT/
 # for testing
-mv Updates/updatez.xml Updates/updates.xml
-ssh osric chmod -R go+rX web/nriley/software/$PRODUCT
+mv $APPCAST_TESTING $APPCAST
+ssh osric chmod -R go+rX $WEBDIR/$PRODUCT
